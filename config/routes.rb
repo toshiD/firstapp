52378769897f0cb8eab9e0f2286d0891fae51d8f
toshi <strategy.is.in.the.detail.1121@gmail.com>
2017-03-14 21:12:37 +0900

    stuck in merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   
+<<<<<<< HEAD
+  get 'user/new'
+
+  get 'static_pages/home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+
+=======
+  resources :users
+>>>>>>> modeling-users
   resources :articles do
   	resources :comments
   end
