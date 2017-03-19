7712fab0221fd2fa7ce14b9069e340b252927f31
toshi <strategy.is.in.the.detail.1121@gmail.com>
2017-03-19 21:56:33 +0900

    finished chapter 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311113554) do
+ActiveRecord::Schema.define(version: 20170319025320) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170311113554) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
