7810ddf06c8f88a94587df66a11279a9acd17773
toshi <strategy.is.in.the.detail.1121@gmail.com>
2017-03-26 10:00:03 +0900

    finished 12

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'user/new'
 
   get 'static_pages/home'
@@ -16,6 +20,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 
   resources :articles do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322131529) do
+ActiveRecord::Schema.define(version: 20170325121016) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20170322131529) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
