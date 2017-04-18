4c5d6c24cf3d54cc81fdd035e6735136fc55cc2d
toshi <strategy.is.in.the.detail.1121@gmail.com>
2017-04-18 21:37:00 +0900

    finished 2nd tutorial

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,6 @@ class MicropostTest < ActiveSupport::TestCase
   
   def setup
     @user = users(:michael)
-    # This code is not idiomatically correct.
     @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
